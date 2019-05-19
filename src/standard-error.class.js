61b73dd832a8fb21084b127b9005106fd4eb2540
Przemysław Chudziński <przemyslaw-chudzinski@wp.pl>
2019-05-19 17:51:16 +0200

    create code comments

--- src/standard-error.class.js
+++ src/standard-error.class.js
@@ -4,14 +4,20 @@ const _message = new WeakMap();
 class StandardError {
 
     constructor(message) {
+        // Validate entry values
         if (typeof message !== "string") {
             throw new Error('message must be string type');
         }
 
+        // Assign private members
         _message.set(this, message);
 
     }
 
+    /**
+     * @desc Returns error message
+     * @return {string}
+     */
     get message() {
         return _message.get(this);
     }

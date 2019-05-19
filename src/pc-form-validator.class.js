4cc077ef68d2d3d4fddd915e550549c5e0d76f71
Przemysław Chudziński <przemyslaw-chudzinski@wp.pl>
2019-05-19 19:01:45 +0200

    refactoring

--- src/pc-form-validator.class.js
+++ src/pc-form-validator.class.js
@@ -1,16 +1,23 @@
 const controlTypes = require('./control-types');
 const validationStates = require('./validation-states');
+const controlStates = require('./control-states');
+const {ExtraValidationStack} = require('./stacks');
+const StandardError = require('./standard-error.class');
 const {
     InputControl,
     InputEmailControl,
     InputCheckboxControl
 } = require('./form-controls');
+const {
+    getFormControls,
+    setNovalidateAttribute
+} = require('./form-validator-support');
 // Private members
 const _formElem = new WeakMap();
 const _stackControls = new WeakMap();
+const _extraValidationStack = new WeakMap();
 // Private methods
 const _setup = Symbol();
-const _getFormControls = Symbol();
 const _initFormControls = Symbol();
 const _assignEvents = Symbol();
 const _handleSubmit = Symbol();
@@ -20,14 +27,18 @@ class PcFormValidator {
 
     constructor(selector) {
         const formElem = document.querySelector(selector);
+
         if (!formElem) {
-            throw new Error('There is not any form which match to given selector');
+            throw new Error('There is not any form which match to the given selector');
         }
         // Assign form to the private member
         _formElem.set(this, formElem);
 
         // Init stack of the controls
         _stackControls.set(this, []);
+
+        // Init extra validation stack
+        _extraValidationStack.set(this, new ExtraValidationStack());
 
         // Prepare form
         this[_setup]();
@@ -37,72 +48,80 @@ class PcFormValidator {
     }
 
     [_setup]() {
-
         // Add novalidate attribute to the form element
-        _formElem.get(this).setAttribute('novalidate', '');
-
+        setNovalidateAttribute(_formElem.get(this));
         this[_initFormControls]();
-
     }
 
     // Assign form events
     [_assignEvents]() {
-
         _formElem.get(this).addEventListener('submit', this[_handleSubmit].bind(this));
-
-    }
-
-    /**
-     * @desc Returns form controls
-     * @return {NodeListOf<SVGElementTagNameMap[string]> | NodeListOf<HTMLElementTagNameMap[string]> | NodeListOf<Element>}
-     */
-    [_getFormControls]() {
-      return _formElem.get(this).querySelectorAll('input[data-form-control], textarea[data-form-control], select[data-form-control]');
     }
 
     [_initFormControls]() {
-        const controls = this[_getFormControls]();
+        const controls = getFormControls(_formElem.get(this));
         !(!controls || !controls.length || !controls.forEach(control => this[_initControl](control)));
     }
 
     [_initControl](control) {
-
         switch (control.type) {
             case controlTypes.EMAIL:
                 return _stackControls.get(this).push(new InputEmailControl(control));
             case controlTypes.CHECKBOX:
                 return _stackControls.get(this).push(new InputCheckboxControl(control));
             default:
-                return _stackControls.get(this).push(new InputControl(control));
+                return _stackControls.get(this).push(new InputControl(control, _extraValidationStack.get(this)));
         }
-
     }
 
     [_handleSubmit](event) {
-
         event.preventDefault();
-
         this.validate() === validationStates.VALID && _formElem.get(this).submit();
-
     }
 
     // Public API
 
     validate() {
         let validationState = validationStates.VALID;
-
         _stackControls.get(this).forEach(controlInstance => controlInstance.validate() === validationStates.INVALID && (validationState = validationStates.INVALID));
-
         return validationState;
     }
 
+    /**
+     * @desc Resets all form controls
+     */
     reset() {
         _stackControls.get(this).forEach(controlInstance => controlInstance.reset());
     }
 
-    // destroy() {
-    //
-    // }
+    /**
+     * @desc Registers new validation rule
+     * @param attr
+     * @param validator
+     */
+    registerRule(attr, validator = () => {}) {
+        if (typeof attr !== "string") {
+            throw new Error('attr name must be type of string');
+        }
+
+        // Push to the extra validation stack new registered rule
+        _extraValidationStack.get(this).push({
+            attr,
+            validator
+        });
+    }
+
+    /**
+     * @desc Returns support object
+     * @return {{StandardError: StandardError, controlStates: ({DIRTY, TOUCHED, PRISTINE}|*), validationStates: ({VALID, INVALID}|*)}}
+     */
+    get support() {
+        return {
+            validationStates,
+            controlStates,
+            StandardError
+        };
+    }
 
 }
 

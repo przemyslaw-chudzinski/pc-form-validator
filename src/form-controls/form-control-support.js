a21549c920e9bc1331cf139e9910e4c4c3cd4ce8
Przemysław Chudziński <przemyslaw-chudzinski@wp.pl>
2019-05-19 21:41:43 +0200

    extracted css classes names to new file

--- src/form-controls/form-control-support.js
+++ src/form-controls/form-control-support.js
@@ -1,4 +1,5 @@
 const StandardError = require('../standard-error.class');
+const cssClasses = require('../css-classes');
 
 const validateState = (state, stack = {}) => {
     const result = Object.values(stack).some(s => s === state);
@@ -8,7 +9,7 @@ const validateState = (state, stack = {}) => {
 };
 
 const clearErrorLabels = (control) => {
-    const errorLabels = control.parentNode.querySelectorAll('span.error-label');
+    const errorLabels = control.parentNode.querySelectorAll('span.' + cssClasses.errorLabel);
     errorLabels.length && [].forEach.call(errorLabels, lbl => lbl.remove());
 };
 
@@ -17,7 +18,7 @@ const createErrorLabel = error => {
         throw new Error('error must an instance of the StandardError class');
     }
     const errorLabel = document.createElement('span');
-    errorLabel.classList.add('error-label');
+    errorLabel.classList.add(cssClasses.errorLabel);
     errorLabel.innerText = error.message;
     return errorLabel;
 };
